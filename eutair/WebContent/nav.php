25e161adb4552d3bdd48e677798a633f44332729
Anuj Gupta <anuj.gupta22ga@gmail.com>
2023-07-09 14:25:07 +0530

    initial nav

---
+++ eutair/WebContent/nav.php
@@ -0,0 +1,108 @@
+<nav
+		class="navbar navbar-default navbar-trans navbar-expand-lg fixed-top">
+		<div
+			class="container"
+			style="margin-right: unset;">
+			<button
+				class="navbar-toggler collapsed"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarDefault"
+				aria-controls="navbarDefault"
+				aria-expanded="false"
+				aria-label="Toggle navigation">
+				<span></span> <span></span> <span></span>
+			</button>
+			<logo></logo>
+			<div
+				class="navbar-collapse collapse justify-content-center"
+				id="navbarDefault">
+				<ul class="navbar-nav">
+					<li class="nav-item"><a
+						class="nav-link"
+						href="index.html">Home</a></li>
+					<li class="nav-item dropdown"><a
+						class="nav-link dropdown-toggle"
+						href="#"
+						id="navbarDropdown-about"
+						role="button"
+						data-toggle="dropdown"
+						aria-haspopup="true"
+						aria-expanded="false"> About Us </a>
+						<div
+							class="dropdown-menu"
+							aria-labelledby="navbarDropdown-about">
+							<a
+								class="dropdown-item"
+								href="who-we-are.html">Who We Are</a> <a
+								class="dropdown-item"
+								href="press-release-certificate.html">Press Release &
+								Certification</a>
+						</div></li>
+					<li class="nav-item dropdown"><a
+						class="nav-link dropdown-toggle"
+						href="#"
+						id="navbarDropdown-product"
+						role="button"
+						data-toggle="dropdown"
+						aria-haspopup="true"
+						aria-expanded="false"> Products </a>
+						<div
+							class="dropdown-menu"
+							aria-labelledby="navbarDropdown-product">
+							<a
+								class="dropdown-item"
+								href="screw-air-compressor.html">Screw Air Compressor<img
+								alt="Screw Air Compressor"
+								src="img/eut-prod/sm-mark-compressors_logo.jpg" />
+							</a> <a
+								class="dropdown-item"
+								href="refrigerated-air-dryer.html">Refrigerated Air Dryers<img
+								alt="Refrigerated Air Dryers"
+								src="img/eut-prod/sm-mark-compressors_logo.jpg" />
+							</a> <a
+								class="dropdown-item"
+								href="reciprocating-compressor-and-dryer.html">Reciprocating
+								Compressor & Dryer</a> <a
+								class="dropdown-item"
+								href="spare-parts.html">Spare Parts</a> <a
+								class="dropdown-item"
+								href="pressure-vessel.html">Pressure Vessel</a>
+						</div></li>
+					<li class="nav-item dropdown"><a
+						class="nav-link dropdown-toggle"
+						href="#"
+						id="navbarDropdown-service"
+						role="button"
+						data-toggle="dropdown"
+						aria-haspopup="true"
+						aria-expanded="false">Services</a>
+						<div
+							class="dropdown-menu"
+							aria-labelledby="navbarDropdown-service">
+							<a
+								class="dropdown-item"
+								href="amc-service.html">Annual Maintenance Contract</a> <a
+								class="dropdown-item"
+								href="engineering-consultancy-service.html">Engineering
+								Consultancy</a> <a
+								class="dropdown-item"
+								href="epc-turnkey-service.html">EPC-Turnkey Solutions</a> <a
+								class="dropdown-item active"
+								href="air-audit-service.html">Air Audits</a> <a
+								class="dropdown-item"
+								href="customization-service.html">Product Customization</a> <a
+								class="dropdown-item"
+								href="training-workshop-service.html">Seminars, Webinars &
+								Workshops</a><a
+								class="dropdown-item"
+								href="waste-water-engineering.html">Wastewater Engineering/a>
+						</div></li>
+					<li class="nav-item"><a
+						class="nav-link"
+						href="contact.html">Contact</a></li>
+				</ul>
+			</div>
+		</div>
+		<contact-info class="contact-info-custom"></contact-info>
+	</nav>
